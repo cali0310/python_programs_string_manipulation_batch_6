6ee92a29c9591c22b070898164ffb9cab15f1ed3
unknown <ramosyazmien2@gmail.com>
2025-03-28 23:59:48 +0800

    pseudocode

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -1,3 +1,13 @@
 # input string
 # remove spaces at the beginning of the string
 # print string
+
+def remove_space(string):
+    for i in range(len(string)):
+        if string[i] != " ":
+            return string[i:]
+    return ""
+
+user_input_string = str(input("Enter string: "))
+no_space = remove_space(user_input_string)
+print("result:", no_space)
