b13f899983208493e8b3b8ecbe753e1ff2f08cef
unknown <ramosyazmien2@gmail.com>
2025-03-29 00:18:39 +0800

    pseudocode

---
+++ left_justify_without_ljust.py
@@ -0,0 +1,6 @@
+# Create a program that do the same functionality without using ljust() function.
+# ask user input
+# Calculate the length of string
+# If the length is less than width
+# Append (width - length) spaces to string
+# Print the left-justified string
