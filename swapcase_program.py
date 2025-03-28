2ee9f3e72ebdd6a5d6590aa9a3bf31fd2a0802c9
unknown <ramosyazmien2@gmail.com>
2025-03-29 00:26:15 +0800

    pseudocode

---
+++ swapcase_program.py
@@ -0,0 +1,4 @@
+# Create a program that does the same functionality without using swapcase() function.
+# Ask user input
+# Convert uppercase letters to lowercase and lowercase letters to uppercase
+# Print the swapped case string
