a47856c9094eded22893a33d525c502fc501dc19
unknown <ramosyazmien2@gmail.com>
2025-03-29 00:28:28 +0800

    pseudocode

---
+++ capitalize_program.py
@@ -0,0 +1,5 @@
+# Create a program that does the same functionality without using capitalize() function.
+# Ask user input
+# Convert the first character to uppercase
+# Convert all other characters to lowercase
+# Print the capitalized string
