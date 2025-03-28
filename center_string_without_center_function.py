c21a0d9e0ac7877eec01740ca1ec025e29574f37
unknown <ramosyazmien2@gmail.com>
2025-03-29 00:24:51 +0800

    final output

--- center_string_without_center_function.py
+++ center_string_without_center_function.py
@@ -3,3 +3,13 @@
 # ask width
 # center the string
 # print string
+
+def custom_center(string, width):
+    total_padding = max(0, width - len(string))
+    left_padding = total_padding // 2
+    right_padding = total_padding - left_padding
+    return ' ' * left_padding + string + ' ' * right_padding
+
+statement = input("Enter statement: ")
+width = int(input("Enter total width: "))
+print(f'"{custom_center(statement, width)}"')
